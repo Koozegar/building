34a3d577a51002d6497ebee2b6d7280e3a6e25b2
Koozegar <mrteimooryo@gmail.com>
2024-05-08 11:48:00 +0330

    rest for mohammad

--- frontend/src/component/Select-input-project.jsx
+++ frontend/src/component/Select-input-project.jsx
@@ -1,5 +1,17 @@
 // salam azizan
 
-export default function (){
-   <button></button>
+// export default function (){
+//    <button>salam</button>
+// }
+
+import React from 'react'
+
+const Selectinputproject = () => {
+  return (
+    <div>
+      
+    </div>
+  )
 }
+
+export default Selectinputproject

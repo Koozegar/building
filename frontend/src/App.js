1d2d0304c63f582b1714dc80d11bedc32e60304a
Ali <haghi.tarooni@gmail.com>
2024-05-10 15:09:46 -0700

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,17 @@ import Grid from '@mui/material/Grid';
 import './App.css';
 import LoginSignup from "./component/login-signup";
 import AddJobReport from "./component/AdJobRep"
-import MainView from './component/building-mainview';
+import CustomizedSnackbars from './component/Snackbar';
+import SelectProjectPage from './component/Select-Project-Page';
+import WorkView from './component/create-work-page';
+import CreateProjectPage from './component/CreateProjectPage/CreateProjectPage'
 
 
 function App() {
 
     return (
-            <MainView/>
-            
+        <CreateProjectPage>
+        </CreateProjectPage>            
         
     )
 }
